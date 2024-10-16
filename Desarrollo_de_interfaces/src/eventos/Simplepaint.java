dfd97af7533e47fc5c48b9e9b6ac2146b607084a
Grffita <alvaroblanco496@gmail.com>
2024-10-16 14:00:18 +0200

    1.2 Simplepaint.java- 16/10

--- Desarrollo_de_interfaces/src/eventos/Simplepaint.java
+++ Desarrollo_de_interfaces/src/eventos/Simplepaint.java
@@ -1,81 +1,67 @@
-package eventos;
-
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.LayoutManager;
-import java.awt.Point;
+package Interfaces;
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionAdapter;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-
+import java.util.ArrayList;
+import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+public class EjercicioPaintArreglar extends JPanel {
 
-public class Simplepaint extends JPanel {
-    private List<Point> points = new ArrayList<>();
-    private List <List<Point>> puntos = new ArrayList<>();
-
-    public Simplepaint() {
-
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent e) {
-                points = new ArrayList<>();  // Comienza un nuevo trazo
-                points.add(e.getPoint());
-                repaint();
-            }
-
-            @Override
-            public void mouseReleased(MouseEvent e) {
-                puntos.add(points);  // Almacena el trazo actual
-            }
-        });
-
-        addMouseMotionListener(new MouseMotionAdapter() {
-            @Override
-            public void mouseDragged(MouseEvent e) {
-                points.add(e.getPoint());
-                repaint();
-            }
-        });
-    }
-
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        g.setColor(Color.black);
-
-        // Dibuja todas las líneas guardadas en "puntos"
-        for (List<Point> trazo : puntos) {
-            for (int i = 1; i < trazo.size(); i++) {
-                Point p1 = trazo.get(i - 1);
-                Point p2 = trazo.get(i);
-                g.drawLine(p1.x, p1.y, p2.x, p2.y);
-            }
-        }
-
-        // Dibuja el trazo actual
-        for (int i = 1; i < points.size(); i++) {
-            Point p1 = points.get(i - 1);
-            Point p2 = points.get(i);
-            g.drawLine(p1.x, p1.y, p2.x, p2.y);
-           // g.drawOval(i, i, i, i);
-        }
-    }
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Paint");
-        Simplepaint paintPanel = new Simplepaint();
-        frame.add(paintPanel);
-        frame.setSize(400, 200);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
+	//private List<Linea> lineas = new ArrayList<>(); // Lista para almacenar los puntos dibujados
+	private Linea linea = new Linea();
+	
+	public EjercicioPaintArreglar() throws HeadlessException {
+		
+		// Manejador del evento cuando el mouse es presionado
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				linea.points.add(e.getPoint()); // Agrega el punto donde se presionó el mouse
+				repaint(); // Redibuja el panel
+			}
+		});
+		
+		// Manejador del evento cuando el mouse se mueve con el botón presionado
+		addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				linea.points.add(e.getPoint()); // Agrega los puntos donde el mouse se arrastra
+				repaint(); // Redibuja el panel
+			}
+		});
+		
+		addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+		//		lineas.add(linea);
+				linea.points.clear();
+			}
+		});
+	}
+	
+	// Metodo para dibujar los puntos
+	protected void paintComponent(Graphics g) {
+		super.paintComponents(g); // Llama al metodo paintComponent de la clase padre
+		
+		// Establecer el color y dibujar todas las lineas entre puntos
+		g.setColor(Color.BLACK);
+		for (int i = 1; i < linea.points.size(); i++) {
+			Point p1 = linea.points.get (i - 1);
+			Point p2 = linea.points.get(i);
+			g.drawLine(p1.x, p1.y, p2.x, p2.y); // Dibuja líneas entre los puntos consecutivos
+		}
+	}
+	
+	// Crear el JFrame con el lienzo de dibujo
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Paint Basico en Java");
+		EjercicioPaintArreglar paintPannel = new EjercicioPaintArreglar();
+		frame.add(paintPannel);
+		frame.setSize(800, 600); // Tamaño del lienzo
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true); // Mostrar la ventana
+	}
 
 }
